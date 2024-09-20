7cb8bd84bc2232f1707a8e4462112ffe7c59c81e
Leonardo <leonardosilva1360@gmail.com>
2024-09-20 14:32:00 -0300

    feat(Test): Add testes para AnimeService

---
+++ src/test/java/dev/leo/api_anime/service/AnimeServiceTest.java
@@ -0,0 +1,41 @@
+package dev.leo.api_anime.service;
+
+import static org.mockito.Mockito.when;
+
+import java.time.LocalDate;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import dev.leo.api_anime.domain.anime.Anime;
+import dev.leo.api_anime.dto.AnimeDto;
+import dev.leo.api_anime.repository.AnimeRepository;
+
+@ExtendWith(MockitoExtension.class)
+public class AnimeServiceTest {
+
+    @Mock
+    private AnimeRepository animeRepository;
+
+    @InjectMocks
+    private AnimeService animeService;
+
+    @Test
+    public void AnimeServiceTest_save_returnsAnime(){
+        AnimeDto dto = new AnimeDto("Teste anime","Teste anime","Anime para teste", LocalDate.now());
+        Anime anime = dto.toAnime();
+
+        when(animeRepository.save(Mockito.any(Anime.class))).thenReturn(anime); //When the repo.save is called passing an Anime mockito will forcefully return our anime variable
+
+        Anime newAnime = animeService.save(dto);
+      
+        Assertions.assertThat(newAnime).isNotNull();
+        
+    }
+
+}
