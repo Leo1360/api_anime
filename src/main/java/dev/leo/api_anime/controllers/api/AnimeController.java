010edccb6866fc3f65e3ab486adf665a69531a74
Leonardo <leonardosilva1360@gmail.com>
2024-10-03 21:36:36 -0300

    feat: Add update no controller de Anime

--- src/main/java/dev/leo/api_anime/controllers/api/AnimeController.java
+++ src/main/java/dev/leo/api_anime/controllers/api/AnimeController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -45,5 +46,10 @@ public class AnimeController {
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
+    @PutMapping(path = "/anime/{id}")
+    public ResponseEntity<Void> updateAnime(@RequestBody AnimeDto dto,@PathVariable Long id){
+        animeService.update(dto, id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 
 }
