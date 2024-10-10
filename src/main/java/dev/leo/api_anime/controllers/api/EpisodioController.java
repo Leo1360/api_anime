442d525845acd30be64ca6473fea0c63be86cf5f
Leonardo <leonardosilva1360@gmail.com>
2024-10-10 08:42:11 -0300

    fix: Mudando Controllers para RestControllers

--- src/main/java/dev/leo/api_anime/controllers/api/EpisodioController.java
+++ src/main/java/dev/leo/api_anime/controllers/api/EpisodioController.java
@@ -2,7 +2,6 @@ package dev.leo.api_anime.controllers.api;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +10,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import dev.leo.api_anime.domain.anime.Episodio;
 import dev.leo.api_anime.dto.PageDTO;
@@ -21,7 +21,7 @@ import dev.leo.api_anime.service.EpisodioService;
 import lombok.RequiredArgsConstructor;
 
 
-@Controller
+@RestController
 @RequestMapping(path = "/api/episodios")
 @RequiredArgsConstructor
 public class EpisodioController {
